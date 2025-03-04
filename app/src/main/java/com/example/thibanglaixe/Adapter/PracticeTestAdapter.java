4895e0c696393947b53c9cdda58f7e6aeef0edad
DinhQuy <hdquy2003@gmail.com>
2025-03-04 15:56:48 +0700

    practice test adapter

---
+++ app/src/main/java/com/example/thibanglaixe/Adapter/PracticeTestAdapter.java
@@ -0,0 +1,56 @@
+package com.example.thibanglaixe.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.thibanglaixe.R;
+import com.example.thibanglaixe.activity.ViewExamActivity;
+import com.example.thibanglaixe.model.Exam;
+
+import java.util.ArrayList;
+
+public class PracticeTestAdapter extends RecyclerView.Adapter<PracticeTestAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<Exam> listExaml;
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_practice_test, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Exam getExam = listExaml.get(position);
+        holder.title.setText(getExam.getTitle());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewExam = new Intent(holder.itemView.getContext(), ViewExamActivity.class);
+                context.startActivity(viewExam);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listExaml.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.item_tv_practiceTest);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/thibanglaixe/model/Exam.java
@@ -0,0 +1,26 @@
+package com.example.thibanglaixe.model;
+
+public class Exam {
+    String title, code;
+
+    public Exam(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
